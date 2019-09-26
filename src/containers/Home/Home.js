86353a7ff6abceed88ba00fa80915179fc040274
jollyjerr <jollyjerr@gmail.com>
2019-09-26 15:45:06 -0600

    imports react-cytoscapejs and sets HTMLCanvas size based on viewport

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import StellarSystem from '../../components/StellarSystem/StellarSystem'
+
+export default function Home() {
+
+    return (
+        <div>
+            <StellarSystem />
+        </div>
+    )
+}
