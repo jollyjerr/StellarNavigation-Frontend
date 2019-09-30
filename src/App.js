3694ce7c39a3981f597434164035e29d4739dfd3
jollyjerr <jollyjerr@gmail.com>
2019-09-30 11:21:06 -0600

    adds router and home path

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 import './App.css';
 
@@ -26,8 +27,16 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <Home stellarSystems={this.state.stellarSystems} />
+        <Router>
+
+          <Navbar />
+
+          <Route path="/" exact>
+            <Home stellarSystems={this.state.stellarSystems} />
+          </Route>
+
+
+        </Router>
       </div>
     )
   }
