9cb7ad03e87edef487c23cd9f9fddb1303866593
jollyjerr <jollyjerr@gmail.com>
2019-10-01 09:47:55 -0600

    adds navigation menu

--- src/components/StellarSystem/StellarSystem.js
+++ src/components/StellarSystem/StellarSystem.js
@@ -1,36 +1,49 @@
 import React, { Component } from 'react'
 import CytoscapeComponent from 'react-cytoscapejs';
 
+import './StellarSystem.css'
+
 export default class StellarSystem extends Component {
 
     state = {
         w: 0,
         h: 0,
         elements: [
-            { data: { id: 'one', label: 'Node 1' }, position: { x: 45, y: 200 } },
-            { data: { id: 'two', label: 'Node 2' }, position: { x: 500, y: 500 } }
+            { data: { id: 'one', label: 'Sorry' }, position: { x: 45, y: 200 }, style: {color: 'yellow'} },
+            { data: { id: 'two', label: 'Something went wrong' }, position: { x: 500, y: 500 }, style: {color: 'yellow'} }
         ]
     }
 
-    componentDidMount = () => { // this will need refactored if window size changes
+    componentDidMount = () => { 
         fetch(this.props.BACKEND_URL + 'stellarsystem/2') 
         .then(resp => resp.json())
         .then(results => {
                 this.setState({
-                w: window.innerWidth,
+                w: window.innerWidth, // kinda hacky...this will need refactored if window size changes
                 h: window.innerHeight,
                 elements: results
             })
         })
     }
+
+    navigationCards = () => {
+        return this.props.stellarSystem.largeCelestials.map((body, i) => {
+           return <button className="jumper" key={i}> {body.name} </button>
+        })
+    }
+
     
     render() {
         return(
-            <CytoscapeComponent 
-            elements={this.state.elements} 
-            style={{ width: this.state.w, height: this.state.h }}
-
-             />
+            <div>
+                <menu className="navigation-menu" >
+                    {this.props.stellarSystem ?  this.navigationCards() : null}
+                </menu>
+                <CytoscapeComponent 
+                    elements={this.state.elements} 
+                    style={{ width: this.state.w, height: this.state.h }}
+                />
+            </div>
         ) 
     }
 }
